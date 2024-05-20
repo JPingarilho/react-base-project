85b53aff7fa52e80d5fcf91a0ae0922101b715da
GabrielTdj <160868501+GabrielTdj@users.noreply.github.com>
2024-05-20 20:32:05 -0300

    Comecei as alteracoes no style.js

--- src/components/Relatorios/style.js
+++ src/components/Relatorios/style.js
@@ -2,32 +2,59 @@ import styled from "styled-components";
 
 
 const FormContainer = styled.div`
-  width: 380px;
-  border: 0px solid #ccc;
-  padding:300px;
-  border-radius: 5px;
+  width: 80%;
+  max-width: 600px;
+  margin: 50px auto;
+  padding: 30px;
+  border: 1px solid #e3e3e3;
+  border-radius: 10px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+
+  @media (min-width: 768px) {
+    padding: 40px;
+  }
 `;
 
 const Label = styled.label`
   display: block;
-  margin-bottom: 13px;
+  margin-bottom: 10px;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #333;
 `;
-
 
 const Input = styled.input`
   width: 100%;
-  padding: 7px;
+  padding: 12px;
+  margin-bottom: 20px;
   border: 1px solid #ccc;
-  border-radius: 3px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  font-size: 1rem;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
 `;
 
+
 const Button = styled.button`
+  width: 100%;
+  padding: 14px;
   background-color: #007bff;
   color: #fff;
-  padding: 10px 20px;
   border: none;
-  border-radius: 7px;
+  border-radius: 5px;
+  font-size: 1.125rem;
   cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
 `;
 
 export { FormContainer, Label, Input, Button };
