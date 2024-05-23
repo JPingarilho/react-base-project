e0be139bea5f31d8153346d0f51047e3524ead4b
GabrielTdj <160868501+GabrielTdj@users.noreply.github.com>
2024-05-23 01:02:08 -0300

    Update no style.js

--- src/components/Relatorios/style.js
+++ src/components/Relatorios/style.js
@@ -2,15 +2,14 @@ import styled from "styled-components";
 
 
 const FormContainer = styled.div`
-  width: 80%;
-  max-width: 600px;
-  margin: 50px auto;
-  padding: 30px;
+  width: 100%;
+  max-width: 1000px;
+  margin: 200px auto;
+  padding: 3000px;
   border: 1px solid #e3e3e3;
-  border-radius: 10px;
+  border-radius: 35px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   background-color: #ffffff;
-
   @media (min-width: 768px) {
     padding: 40px;
   }
@@ -18,7 +17,8 @@ const FormContainer = styled.div`
 
 const Label = styled.label`
   display: block;
-  margin-bottom: 10px;
+  margin-bottom: 5px auto;
+  margin-top: 7px;
   font-size: 1rem;
   font-weight: bold;
   color: #333;
@@ -28,6 +28,7 @@ const Input = styled.input`
   width: 100%;
   padding: 12px;
   margin-bottom: 20px;
+  margin-top: 20px;
   border: 1px solid #ccc;
   border-radius: 5px;
   box-sizing: border-box;
@@ -44,6 +45,7 @@ const Input = styled.input`
 const Button = styled.button`
   width: 100%;
   padding: 14px;
+  margin-top: 40px;
   background-color: #007bff;
   color: #fff;
   border: none;
